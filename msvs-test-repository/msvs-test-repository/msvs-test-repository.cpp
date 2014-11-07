4dd95a9217536013ed53a6f436223686b486efad
fifosm <github@soleymanimagham.net>
2014-11-06 22:58:50 -0800

    the first visual studio commit test

---
+++ msvs-test-repository/msvs-test-repository/msvs-test-repository.cpp
@@ -0,0 +1,14 @@
+// msvs-test-repository.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello World!");
+	getchar();
+
+	return 0;
+}
+
